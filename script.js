5f7e6e5ed8d44b362098e64df434482c0bbafdb4
WebKawsar <web.kawsarahmed@gmail.com>
2020-07-28 23:14:42 +0600

    finaly finished my asignment-4

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-document.getElementById("fail").style.display = "none";
-document.getElementById("pass").style.display = "none";
+/**************************************************************************************************************
+ *                                      Left Side Pin Generate Button
+ *************************************************************************************************************/
+generateInput = document.getElementById("generate-pin");
+var fail = document.getElementById("fail").style.display = "none";
+var pass = document.getElementById("pass").style.display = "none";
 
 document.getElementById("generate-button").addEventListener("click", function(){
 
     randomNumber = Math.random() * 10000;
     roundNumber = Math.round(randomNumber);
-    generateInput = document.getElementById("generate-value");
+    
 
     if(roundNumber < 10000 && roundNumber > 999){
         
@@ -24,12 +28,91 @@ document.getElementById("generate-button").addEventListener("click", function(){
 
 });
 
+/**************************************************************************************************************
+ *                                      Right Side Pin Matching Button
+ *************************************************************************************************************/
+
+var generateNumber = document.getElementById("generate-number");
+var buttons = document.querySelectorAll(".button");
+var generateShow = "";
+
+for(button of buttons){
+
+    button.addEventListener("click", function(){
+
+            var getValue = event.target.innerText;
+            var number = parseInt(getValue);
+
+        if(getValue === "C" || getValue === "<" ){
+
+            if(getValue === "C"){
+
+                generateShow = "";
+                generateNumber.value = "";
+
+            }
+            else if(getValue === "<" ) {
+
+                
+                var deleteOne = generateNumber.value;
+                if(deleteOne.length > 0){
+                    
+                    get = deleteOne.slice(0, -1);
+                    generateNumber.value = get;
+                    generateShow = get;
+
+                }
+                else {
+
+                    generateShow = "";
+                    return;
+                }
+
+                console.log(deleteOne);
+            
+            }
+            
+        }
+        else if(number === 0 || number === 1 || number === 2 || number === 3 || number === 4 || number === 5 || number === 6 || number === 7 || number === 8 || number === 9){
+
+            generateShow =  generateShow + number;
+            generateNumber.value = generateShow;
+
+        }
+        
+
+    });
 
 
+}
 
 
+//Submit button click
+document.getElementById("submit").addEventListener("click", function(){
+
+            generateInput = document.getElementById("generate-pin");
+            generateNumber = document.getElementById("generate-number");
+
+            leftSideValue = parseInt(generateInput.value);
+            rightSideValue = parseInt(generateNumber.value);
+
+            if(leftSideValue == rightSideValue){
+
+                document.getElementById("pass").style.display = "block";
+                document.getElementById("fail").style.display = "none";
+
+                // pin matched on block
+                // document.getElementById("box").style.display = "block";
+                
+            }
+            else {
+
+                document.getElementById("fail").style.display = "block";
+                document.getElementById("pass").style.display = "none";
+            }
 
 
+});
 
 
 
